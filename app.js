ce25e291a6e903cab731b50158f89eced3448056
jasn417 <jasn417@hotmail.com>
2025-03-04 16:35:55 -0500

    agragando lista de amigos

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
-
+let nombre = document.querySelector("input")
+let listaAmigos = document.getElementById("listaAmigos")
 
 let nombres = [];
 
 function agregarAmigo(){
-    let nombre = document.querySelector("input")
+    
     let agregarNombre = nombre.value;
     console.log(agregarNombre) 
     console.log(nombres)
@@ -14,10 +14,24 @@ function agregarAmigo(){
         alert("Por favor, inserte un nombre.");
     }else{
         nombres.push(agregarNombre);
+        liNombre ();
+        limpiarInput();
     }
-    limpiarInput();
+    
 } 
 
 function limpiarInput(){
     document.querySelector("#amigo").value = "";
 }
+
+ function liNombre (){
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < nombres.length; i++){
+        let li = document.createElement("li");
+        li.innerHTML = nombres[i];
+        listaAmigos.appendChild(li);
+    }
+ }
+
+ 
